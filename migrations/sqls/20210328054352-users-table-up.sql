1968f54c09b5790f6251b71fb46f356b68d73de7
Ananta Kr. Roy <kr.innovate@gmail.com>
2021-03-29 16:50:16 +0530

    Add non null constraints

--- migrations/sqls/20210328054352-users-table-up.sql
+++ migrations/sqls/20210328054352-users-table-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id serial PRIMARY KEY,
-    first_name VARCHAR,
-    last_name VARCHAR,
-    password_digest VARCHAR
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    password_digest VARCHAR NOT NULL
 );
--- migrations/sqls/20210328061539-order-products-table-up.sql
+++ migrations/sqls/20210328061539-order-products-table-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE order_products(
     id serial PRIMARY KEY,
-    quantity INTEGER,
+    quantity INTEGER NOT NULL CHECK(quantity > 0),
     order_id bigint REFERENCES orders(id),
     product_id bigint REFERENCES products(id)
 );
